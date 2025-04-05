7dbfd411654edeb9d256c8fa43aa1940377c8fbc
Kaushik Ram <22810981+Kaushik-A-Ram@users.noreply.github.com>
2025-04-05 16:49:01 +1100

    added properties

---
+++ script.js
@@ -0,0 +1,9 @@
+const myMovie = {
+    Title: 'The longest day',
+    Year: 1964,
+}
+
+myMovie.Rating = 5;
+myMovie.Summary = "World War II movie about the Normandy landings";
+
+console.log(myMovie);
